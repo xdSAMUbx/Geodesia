6bdd602e39c690274fb0879e9336eaf43a359834
Samuel Calderon <samuelcalderon1202@hotmail.com>
2024-02-14 23:09:22 -0500

    pag2

--- Pagina Web/static/script.js
+++ Pagina Web/static/script.js
@@ -1,9 +1,12 @@
 
 const mundo = document.getElementById("mundo");
 const barraLateral = document.querySelector(".barra-lateral")
-const span = document.querySelector("span")
+const spans = document.querySelectorAll("span")
 
 mundo.addEventListener("click",()=>{
     barraLateral.classList.toggle("mini-barra-lateral");
-    span.classList.toggle("oculto")
+    
+    spans.forEach((span)=>{
+        span.classList.toggle("oculto");
+    });
 });
