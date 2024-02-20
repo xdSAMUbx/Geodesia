654a7fdd2f9d7ac724686e86736aab1246db96af
Samuel Calderon <samuelcalderon1202@hotmail.com>
2024-02-20 10:36:54 -0500

    cor

--- Geodesia_General/Latitudes.py
+++ Geodesia_General/Latitudes.py
@@ -16,14 +16,23 @@ class Latitudes:
         self.geocentrica = mh.degrees(mh.atan((1-self.e)*(mh.tan(mh.radians(self.geodesica)))))
         self.reducida = mh.degrees(mh.atan((mh.sqrt(1-self.e))*(mh.tan(mh.radians(self.geodesica)))))
 
+        print(f'La coordenada geocentrica es: {self.geocentrica}')
+        print(f'La coordenada reducida es: {self.geocentrica}')
+
     def geocen(self):
 
         b = self.a*mh.sqrt(1-self.e)
         self.geodesica = mh.degrees(mh.atan((mh.tan(mh.radians(self.geocentrica)))/1-self.e))
         self.reducida = mh.degrees((self.a/b)*(mh.tan(self.geocentrica)))
 
+        print(f'La coordenada geodesica es: {self.geodesica}')
+        print(f'La coordenada reducida es: {self.geocentrica}')
+
     def reduc(self):
         
         b = self.a*mh.sqrt(1-self.e)
         self.geodesica = mh.degrees(mh.atan((mh.tan(mh.radians(self.reducida)))/mh.sqrt(1-self.e)))
         self.geocentrica = mh.degrees(mh.atan((b/self.a)*(mh.tan(mh.radians(self.reducida)))))
+
+        print(f'La coordenada geocentrica es: {self.geocentrica}')
+        print(f'La coordenada geodesica es: {self.geodesica}')
